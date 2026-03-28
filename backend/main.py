0942f013b98fef9d3a346239c6097d9b97d976e4
Agu1406 <agustin6041@outlook.es>
2026-03-28 04:31:02 +0100

    Refactor CORS origins list in main.py for improved readability and consistency. Update string formatting for URLs.

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 # Importamos el middleware necesario para hacer CORS entre frontend y backend.
 from fastapi.middleware.cors import CORSMiddleware
 
-origin = [
-    https://agustinmarquez.dev
-    https://www.agustinmarquez.dev
-    http://localhost:3000 # Para desarrollo / pruebas en local.
-    # https://api.agustinmarquez.dev (es la de origen, no se pone).
+origins = [
+    "https://agustinmarquez.dev",
+    "https://www.agustinmarquez.dev",
+    "http://localhost:3000", # Para desarrollo / pruebas en local.
+    # "https://api.agustinmarquez.dev" (es la de origen, no se pone).
 ]
 
 # Instanciamos un objeto del tipo FastAPI.
