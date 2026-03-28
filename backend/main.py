9cda67e71919d861f8bda2efbaa32d92f0ecc92e
Agu1406 <agustin6041@outlook.es>
2026-03-28 04:29:33 +0100

    Add CORS middleware to FastAPI application for handling cross-origin requests. Update origins list for development and production environments.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 # Importamos FastAPI con un nombre personalizado similar pero en PascualCase.
 from fastapi import FastAPI
+# Importamos el middleware necesario para hacer CORS entre frontend y backend.
+from fastapi.middleware.cors import CORSMiddleware
+
+origin = [
+    https://agustinmarquez.dev
+    https://www.agustinmarquez.dev
+    http://localhost:3000 # Para desarrollo / pruebas en local.
+    # https://api.agustinmarquez.dev (es la de origen, no se pone).
+]
 
 # Instanciamos un objeto del tipo FastAPI.
 app = FastAPI()
+
+# Configuración del comportamiento del middleware en nuestra aplicación.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Ejemplo: La raíz de la API devuelve un JSON con "Hello World".
 @app.get("/")
