aea2280a1a842904efae81a2672c73bffe85591a
Agu1406 <agustin6041@outlook.es>
2026-05-12 23:27:16 +0200

    Fix (main page.tsx): Author link in cookie was missing the URL-end with my profile information.

--- frontend/app/(pages)/cookies/page.tsx
+++ frontend/app/(pages)/cookies/page.tsx
@@ -1,7 +1,7 @@
 /**
  * Página de Política de Cookies para agustinmarquez.dev.
  *
- * @author Agu1406 (https://github.com)
+ * @author Agu1406 (https://github.com/Agu1406)
  * @version 1.0.0
  * @returns {JSX.Element}
  */
